ef1ea6edbc1987e33070acdbc0b40d47eae93186
정해성 <gotjd0230@naver.com>
2025-02-06 23:08:04 +0900

    chore: 포메팅 의존성 추가

--- src/components/Map/SideNav.tsx
+++ src/components/Map/SideNav.tsx
@@ -7,10 +7,10 @@ export function SideNav() {
   if (!isNavOpen) return null;
 
   return (
-    <div className="fixed left-0 top-[65px] h-[calc(100vh-65px)] w-80 bg-white shadow-lg z-10">
+    <div className="fixed left-0 top-[65px] z-10 h-[calc(100vh-65px)] w-80 bg-white shadow-lg">
       <button
         onClick={closeNav}
-        className="absolute flex items-center justify-center w-10 h-10 text-white bg-red-500 rounded-none size-4 top-4 right-4"
+        className="absolute flex items-center justify-center w-10 h-10 text-white bg-red-500 rounded-none right-4 top-4 size-4"
         style={{ boxSizing: 'border-box' }}
       >
         ✕
