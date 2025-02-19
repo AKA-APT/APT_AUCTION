4c7961e8e26718dd57b5b801330620c1ac83697b
정해성 <gotjd0230@naver.com>
2025-02-19 23:28:40 +0900

    feat: 클러스터 훅 분리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,24 +3,11 @@ import { useInitializeMap } from '@/hooks/Map/useInitializeMap';
 import { useSetMarker } from '@/hooks/Map/useSetMarker';
 import { useAuctions } from '@/hooks/queries/useAuctions';
 import { Suspense, useEffect, useState } from 'react';
-import clusterUrl1 from '@/assets/cluster/cluster-marker-1.png';
-import clusterUrl2 from '@/assets/cluster/cluster-marker-2.png';
-import clusterUrl3 from '@/assets/cluster/cluster-marker-3.png';
-import clusterUrl4 from '@/assets/cluster/cluster-marker-4.png';
-import clusterUrl5 from '@/assets/cluster/cluster-marker-5.png';
 import MarkerClustering from '@/utils/MarkerClustering';
-
-const clusterUrls = [
-  clusterUrl1,
-  clusterUrl2,
-  clusterUrl3,
-  clusterUrl4,
-  clusterUrl5,
-];
+import { useSetCluster } from '@/hooks/Map/useSetCluster';
 
 function MapRenderer() {
   useInitializeMap();
-
   return null;
 }
 
@@ -38,36 +25,11 @@ function MerkerRenderer() {
   const { data: auctions } = useAuctions({ lbLat, lbLng, rtLat, rtLng });
 
   const { setMarkers } = useSetMarker();
+  const { setCluster } = useSetCluster();
 
   useEffect(() => {
     const markers = setMarkers(auctions);
-
-    new MarkerClustering({
-      minClusterSize: 2,
-      maxZoom: 13,
-      map: map,
-      markers: markers,
-      disableClickZoom: false,
-      gridSize: 120,
-      icons: clusterUrls.map((url) => {
-        const imageIcon: naver.maps.ImageIcon = {
-          url,
-          size: new naver.maps.Size(40, 40),
-          scaledSize: new naver.maps.Size(40, 40),
-        };
-        return imageIcon;
-      }),
-      indexGenerator: [10, 100, 200, 500, 1000],
-      stylingFunction: function (clusterMarker, count) {
-        const container = clusterMarker.getElement();
-        container.style.position = 'relative';
-        container.innerHTML = `
-          <div class="absolute inset-0 z-10 flex items-center justify-center text-white font-bold text-[12px]">
-            ${count}
-          </div>
-        `;
-      },
-    });
+    setCluster({ map, markers });
   }, [auctions, setMarkers]);
 
   useEffect(() => {
