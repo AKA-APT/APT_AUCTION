a690f528b6dd90299047f4b5e5a0746dbbc53812
정해성 <gotjd0230@naver.com>
2025-05-18 17:11:58 +0900

    feat: 만 단위 추가

--- src/hooks/Map/useSetMarker.ts
+++ src/hooks/Map/useSetMarker.ts
@@ -9,6 +9,8 @@ const formatAmount = (amount: number) => {
     return `${(amount / 100000000).toFixed(1)}억`;
   } else if (amount >= 10000000) {
     return `${(amount / 10000000).toFixed(1)}천만`;
+  } else if (amount >= 10000) {
+    return `${(amount / 10000).toFixed(1)}만`;
   }
   return amount.toLocaleString();
 };
