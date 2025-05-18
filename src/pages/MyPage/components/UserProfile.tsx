09ee276453e64d1d6d653f1361ed3440ed3b08fc
정해성 <gotjd0230@naver.com>
2025-05-18 21:18:04 +0900

    design: 프로필 사진 object cover

--- src/pages/MyPage/components/UserProfile.tsx
+++ src/pages/MyPage/components/UserProfile.tsx
@@ -3,12 +3,12 @@ import { useUser } from '@/hooks/Auth/useUser';
 export function UserProfile() {
   const { data: user } = useUser();
   return (
-    <div className="flex items-center justify-between rounded-lg bg-gray-100 p-4 shadow-md">
+    <div className="flex items-center justify-between p-4 bg-gray-100 rounded-lg shadow-md">
       <div className="flex items-center">
         <img
           src={user?.profileImage}
           alt="User Profile"
-          className="mr-4 h-24 w-24 rounded-full"
+          className="object-cover w-24 h-24 mr-4 rounded-full"
         />
         <div>
           <h2 className="text-xl font-bold">{user?.nickname}님</h2>
